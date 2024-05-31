d7836d5a33f41c840a4410f090e1b5016bbb7bae
aasthapetkar <110039384+aasthapetkar@users.noreply.github.com>
2024-05-31 14:05:31 +0530

    Update AverageArray.java

--- AverageArray.java
+++ AverageArray.java
@@ -1,3 +1,5 @@
+//Write a Java program to calculate the average value of array elements.
+
 public class AverageArray {
     public static void main(String[] args) {
         // Define and initialize the array
